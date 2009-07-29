0a70930630c5ee8734d31fe506c2b38dfb618bd2
Ivan Gromov <ivan.gromov@redsolution.ru>
2009-07-29 12:43:45 +0600

    Fixed bug: user without UserEmail can not login

--- emailauth/backends.py
+++ emailauth/backends.py
@@ -17,7 +17,7 @@ class EmailBackend(ModelBackend):
 class FallbackBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=username)
             if (user.check_password(password) and
                 not UserEmail.objects.filter(user=user).count()):
 
